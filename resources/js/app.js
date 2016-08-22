d831d40d281948de4e5644af8fa36ee0a43f425f
Sean ONeal <smoneal@gmail.com>
2016-08-22 16:42:08 -0700

    added js code for rendering map

---
+++ resources/js/app.js
@@ -0,0 +1,48 @@
+//Model 
+var database = {
+
+}
+
+var control = {
+
+}
+
+var view = {
+	userLat: null,
+	userLong: null,
+	initMap: function () {
+		var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            view.userLat = position.coords.latitude;
+            view.userLong = position.coords.longitude;
+
+            this.infoWindow.setPosition(pos);
+            this.infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            view.handleLocationError(true, this.infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          view.handleLocationError(false, this.infoWindow, map.getCenter());
+        }
+	},
+	handleLocationError: function(browserHasGeolocation, infoWindow, pos) {
+		infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+        					  'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+	}
+}
+window.onload = view.initMap();
