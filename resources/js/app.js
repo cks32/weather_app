fa7787757cfdbd4408cc1e6befdcaecbbe225994
Sean ONeal <smoneal@gmail.com>
2016-08-22 17:29:28 -0700

    took out onload

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,7 @@ var control = {
 var view = {
 	userLat: null,
 	userLong: null,
-	initMap: function () {
+	initMap: function() {
 		var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 6
@@ -45,4 +45,3 @@ var view = {
                               'Error: Your browser doesn\'t support geolocation.');
 	}
 }
-window.onload = view.initMap();
