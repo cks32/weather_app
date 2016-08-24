257b7ccb86fe414b0e29f4ab50a59ec47a560baf
Sean ONeal <sean-oneal@users.noreply.github.com>
2016-08-24 10:25:24 -0700

    testing all api's together, forcing http

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,41 @@
 var view = {
-	userLat: 37,
-	userLong: 122,
-	// initMap: function() {
-	// 	var map = new google.maps.Map(document.getElementById('map'), {
- //          center: {lat: -34.397, lng: 150.644},
- //          zoom: 6
- //        });
- //        var infoWindow = new google.maps.InfoWindow({map: map});
+	userLat: null,
+	userLong: null,
+	initMap: function() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
 
- //        // Try HTML5 geolocation.
- //        if (navigator.geolocation) {
- //          navigator.geolocation.getCurrentPosition(function(position) {
- //            var pos = {
- //              lat: position.coords.latitude,
- //              lng: position.coords.longitude
- //            };
- //            view.userLat = position.coords.latitude;
- //            view.userLong = position.coords.longitude;
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            view.userLat = position.coords.latitude;
+            view.userLong = position.coords.longitude;
 
- //            infoWindow.setPosition(pos);
- //            infoWindow.setContent('Location found.');
- //            map.setCenter(pos);
- //            view.weatherFetch();
- //          }, function() {
- //            view.handleLocationError(true, infoWindow, map.getCenter());
- //          });
- //        } else {
- //          // Browser doesn't support Geolocation
- //          view.handleLocationError(false, infoWindow, map.getCenter());
- //        }
-	// },
-	// handleLocationError: function(browserHasGeolocation, infoWindow, pos) {
-	// 	infoWindow.setPosition(pos);
- //        infoWindow.setContent(browserHasGeolocation ?
- //        					  'Error: The Geolocation service failed.' :
- //                              'Error: Your browser doesn\'t support geolocation.');
-	// },
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+            view.weatherFetch();
+          }, function() {
+            view.handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          view.handleLocationError(false, infoWindow, map.getCenter());
+        }
+	},
+	handleLocationError: function(browserHasGeolocation, infoWindow, pos) {
+		infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+        					  'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+	},
   weatherFetch: function(){
     $.ajax({
       url:"https://api.forecast.io/forecast/871ab11d035adf7442dfa8a03179ecda/"+view.userLat+ "," + view.userLong,
@@ -90,6 +90,6 @@ var view = {
     });
   }
 };
-// view.initMap();
-   view.weatherFetch(); 
-   view.marsWeatherFetch();
+view.initMap();
+// view.weatherFetch(); 
+view.marsWeatherFetch();
