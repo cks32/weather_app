df4e46a664700934a3a426e6084b6c8fc9c4905f
Sean ONeal <sean-oneal@users.noreply.github.com>
2016-08-24 20:05:44 -0700

    disabled zoom

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,8 @@ var view = {
 	initMap: function() {
 		var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
-          zoom: 10
+          zoom: 10,
+          zoomcontrol: false
         });
         var infoWindow = new google.maps.InfoWindow({map: map});
 
